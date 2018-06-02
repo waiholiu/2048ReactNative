e419ead4cc2a2f2b0a2eb1d551893b14e4487493
waiholiu <waiholiuEmail>
2018-06-02 15:00:03 +1000

    formatting

--- src/board.js
+++ src/board.js
@@ -1,43 +1,53 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 import Cell from './cell'
 
 class Board extends Component {
     state = {}
     render() {
         return (
-            <View style={{ 
-                flex: 1, flexDirection: 'column', justifyContent:'center'
-            }}>
-                
+            <View style={
+                styles.board
+            }>
+
                 <View style={{ flexDirection: 'row', backgroundColor: 'powderblue' }}>
-                    <Cell text='21'/>
-                    <Cell  text='2'/>
-                    <Cell  text='3'/>
-                    <Cell  text='4'/>
+                    <Cell text='21' />
+                    <Cell text='2' />
+                    <Cell text='3' />
+                    <Cell text='4' />
                 </View>
-                <View style={{ flexDirection: 'row', justifyContent: 'center', height:60, alignItems: 'center', backgroundColor: 'yellow' }}>
-                    <Cell  text='5'/>
-                    <Cell  text='6'/>
-                    <Cell  text='7'/>
-                    <Cell  text='8'/>
+                <View style={{ flexDirection: 'row', backgroundColor: 'yellow' }}>
+                    <Cell text='5' />
+                    <Cell text='6' />
+                    <Cell text='7' />
+                    <Cell text='8' />
                 </View>
-                <View style={{ flexDirection: 'row', justifyContent: 'center', height:60, alignItems: 'flex-end' }}>
-                    <Cell  text='9'/>
-                    <Cell  text='10'/>
-                    <Cell  text='11'/>
-                    <Cell  text='12'/>
+                <View style={{ flexDirection: 'row'}}>
+                    <Cell text='9' />
+                    <Cell text='10' />
+                    <Cell text='11' />
+                    <Cell text='12' />
                 </View>
-                <View style={{ flexDirection: 'row', justifyContent: 'center', height:60, alignItems: 'flex-end' }}>
-                    <Cell  text='9'/>
-                    <Cell  text='10'/>
-                    <Cell  text='11'/>
-                    <Cell  text='12'/>
+                <View style={{ flexDirection: 'row'}}>
+                    <Cell text='9' />
+                    <Cell text='10' />
+                    <Cell text='11' />
+                    <Cell text='12' />
                 </View>
 
             </View>
         );
     }
+
 }
+
+const styles = StyleSheet.create({
+    board: {
+        flex: 1, flexDirection: 'column', justifyContent: 'center'
+    },
+  });
+
+
+
 
 export default Board;
