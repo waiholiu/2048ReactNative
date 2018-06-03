625ac47a5497a59c2035602cc14d581b6a2d6121
waiholiu <waiholiuEmail>
2018-06-03 13:31:01 +1000

    fixed reset button

--- App.js
+++ App.js
@@ -9,26 +9,26 @@ export default class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-        boardValues: [
-          {x: 0, y: 0, value :0},
-          {x: 1, y: 0, value :0},
-          {x: 2, y: 0, value :0},
-          {x: 3, y: 0, value :2},
-          {x: 0, y: 1, value :0},
-          {x: 1, y: 1, value :0},
-          {x: 2, y: 1, value :0},
-          {x: 3, y: 1, value :4},
-          {x: 0, y: 2, value :0},
-          {x: 1, y: 2, value :4},
-          {x: 2, y: 2, value :0},
-          {x: 3, y: 2, value :0},
-          {x: 0, y: 3, value :4},
-          {x: 1, y: 3, value :0},
-          {x: 2, y: 3, value :0},
-          {x: 3, y: 3, value :2}          
-         ]
+      boardValues: [
+        { x: 0, y: 0, value: 0 },
+        { x: 1, y: 0, value: 0 },
+        { x: 2, y: 0, value: 0 },
+        { x: 3, y: 0, value: 2 },
+        { x: 0, y: 1, value: 0 },
+        { x: 1, y: 1, value: 0 },
+        { x: 2, y: 1, value: 0 },
+        { x: 3, y: 1, value: 4 },
+        { x: 0, y: 2, value: 0 },
+        { x: 1, y: 2, value: 4 },
+        { x: 2, y: 2, value: 0 },
+        { x: 3, y: 2, value: 0 },
+        { x: 0, y: 3, value: 4 },
+        { x: 1, y: 3, value: 0 },
+        { x: 2, y: 3, value: 0 },
+        { x: 3, y: 3, value: 2 }
+      ]
     }
-}
+  }
 
 
   render() {
@@ -40,7 +40,7 @@ export default class App extends React.Component {
 
         <Board boardValues={this.state.boardValues} />
         <Button
-          onPress={this.onPressStart}
+          onPress={() => this.onPressStart()}
           title="Start"
           color="#841584"
         />
@@ -49,8 +49,29 @@ export default class App extends React.Component {
     );
   }
 
-  onPressStart(){
-    this.state.setState({boardValues: [[0, 2, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]]});
+  onPressStart() {
+    let base = this;
+    console.log('hello');
+    base.setState({
+      boardValues: [
+        { x: 0, y: 0, value: 0 },
+        { x: 1, y: 0, value: 0 },
+        { x: 2, y: 0, value: 0 },
+        { x: 3, y: 0, value: 2 },
+        { x: 0, y: 1, value: 0 },
+        { x: 1, y: 1, value: 0 },
+        { x: 2, y: 1, value: 0 },
+        { x: 3, y: 1, value: 4 },
+        { x: 0, y: 2, value: 0 },
+        { x: 1, y: 2, value: 4 },
+        { x: 2, y: 2, value: 0 },
+        { x: 3, y: 2, value: 0 },
+        { x: 0, y: 3, value: 4 },
+        { x: 1, y: 3, value: 0 },
+        { x: 2, y: 3, value: 0 },
+        { x: 3, y: 3, value: 2 }
+      ]
+    });
   }
 
   onSwipe(gestureName, gestureState) {
@@ -73,7 +94,7 @@ export default class App extends React.Component {
     }
   }
 
-  swipeUp(){ 
+  swipeUp() {
     // get the 4 groups of cells
     let groupsOfCells = [];
     for (let x = 0; x < 4; x++) {
@@ -82,18 +103,16 @@ export default class App extends React.Component {
 
     groupsOfCells.forEach(group => {
       // find all the zeros and remove them
-      let sortedCells = group.filter(cell => cell.value != 0).sort((a,b) => a.y - b.y)
+      let sortedCells = group.filter(cell => cell.value != 0).sort((a, b) => a.y - b.y)
       let sortedValues = sortedCells.map((cell) => cell.value);
-    
+
+
       // fill out the request
-      for(let i = 0; i < 4; i++)
-      {
-        if(sortedValues[i])
-        {
+      for (let i = 0; i < 4; i++) {
+        if (sortedValues[i]) {
           group[i].value = sortedValues[i]
-        } 
-        else
-        {
+        }
+        else {
           group[i].value = 0;
         }
 
