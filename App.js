3af161dfc37a77bab4ed83bb33d3a5e92fad61cb
waiholiu <waiholiuEmail>
2018-06-13 21:35:34 +1000

    made swiping more responsive

--- App.js
+++ App.js
@@ -36,8 +36,8 @@ export default class App extends React.Component {
   render() {
 
     const config = {
-      velocityThreshold: 0.3,
-      directionalOffsetThreshold: 80
+      velocityThreshold: 0.1,
+      directionalOffsetThreshold: 70
     };
 
 
